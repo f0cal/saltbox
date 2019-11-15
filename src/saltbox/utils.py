c41d084c5eeb070340446f584723a7d7c10a0ebb
Brian Rossa <br@f0cal.com>
2019-11-14 19:09:26 -0500

    Added utils file for holding misc helper functions.

---
+++ src/saltbox/utils.py
@@ -0,0 +1,15 @@
+import yaml
+import re
+
+def loader_factory():
+    path_matcher = re.compile(r'.*\$\{([^}^{]+)\}.*')
+    def path_constructor(loader, node):
+        return os.path.expandvars(node.value)
+    class EnvVarLoader(yaml.SafeLoader):
+        pass
+    EnvVarLoader.add_implicit_resolver('!path', path_matcher, None)
+    EnvVarLoader.add_constructor('!path', path_constructor)
+    return EnvVarLoader
+
+def load_yaml(path):
+    return yaml.load(open(path), Loader=loader_factory())
